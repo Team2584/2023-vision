c0917595a1b17d918c5336d9516d51fd897e6259
Jetson User <patriotrobotics@jetson.jetson>
2023-02-24 15:41:00 -0800

    Code ripped from vision-tests

---
+++ flirCamera.cpp
@@ -0,0 +1,160 @@
+#include "Cameras.h"
+
+using namespace std;
+using namespace cv;
+
+flirCamera::flirCamera(int camNum)
+    : colorFrame{cv::Size(width, height), CV_8UC3}, grayFrame{cv::Size(width, height), CV_8UC1}
+{
+    using namespace Spinnaker;
+    using namespace Spinnaker::GenApi;
+    using namespace Spinnaker::GenICam;
+
+    const int numCameras = flirCamList.GetSize();
+
+    // Set info
+    setDistCoeffs(-0.4435755056, 0.335254035289252, 0.0002605483032105911, 0.001042419159982218,
+                  0.2248450637613325);
+    setCamParams(571, 572, 346, 247);
+
+    // Finish if there are no cameras
+    if (numCameras == 0)
+    {
+        flirCamList.Clear();
+        flirSystem->ReleaseInstance();
+        cout << "No flir cameras found" << endl;
+        // TODO throw
+    }
+
+    if (numCameras < camNum)
+    {
+        cout << "Unable to access specified camera" << endl;
+        // TODO throw
+    }
+
+    // Select camera
+    pCam = flirCamList.GetByIndex(camNum);
+    pCam->Init();
+
+    // Retrieve GenICam nodemap
+    INodeMap &nodeMap = pCam->GetNodeMap();
+
+    // Recieve enumeration node from nodemap
+    CEnumerationPtr ptrAcquisitionMode = nodeMap.GetNode("AcquisitionMode");
+    if (!IsAvailable(ptrAcquisitionMode) || !IsWritable(ptrAcquisitionMode))
+    {
+        cout << "Unable to set acquisition mode to continuous (enum retrieval). Aborting..." << endl
+             << endl;
+        // TODO throw
+    }
+
+    // Retrieve entry node from enumeration node
+    CEnumEntryPtr ptrAcquisitionModeContinuous = ptrAcquisitionMode->GetEntryByName("Continuous");
+    if (!IsAvailable(ptrAcquisitionModeContinuous) || !IsReadable(ptrAcquisitionModeContinuous))
+    {
+        cout << "Unable to set acquisition mode to continuous (entry retrieval). Aborting..."
+             << endl
+             << endl;
+        // TODO throw
+    }
+
+    // Retrieve integer value from entry node
+    const int64_t acquisitionModeContinuous = ptrAcquisitionModeContinuous->GetValue();
+
+    // Set integer value from entry node as new value of enumeration node
+    ptrAcquisitionMode->SetIntValue(acquisitionModeContinuous);
+
+    // Retrieve Stream Parameters device nodemap
+    Spinnaker::GenApi::INodeMap &sNodeMap = pCam->GetTLStreamNodeMap();
+
+    // Retrieve Buffer Handling Mode Information
+    CEnumerationPtr ptrHandlingMode = sNodeMap.GetNode("StreamBufferHandlingMode");
+    if (!IsAvailable(ptrHandlingMode) || !IsWritable(ptrHandlingMode))
+    {
+        cout << "Unable to set Buffer Handling mode (node retrieval). Aborting..." << endl << endl;
+        // TODO throw
+    }
+
+    CEnumEntryPtr ptrHandlingModeEntry = ptrHandlingMode->GetCurrentEntry();
+    if (!IsAvailable(ptrHandlingModeEntry) || !IsReadable(ptrHandlingModeEntry))
+    {
+        cout << "Unable to set Buffer Handling mode (Entry retrieval). Aborting..." << endl << endl;
+        // TODO throw
+    }
+
+    // Set buffer handling mode
+    ptrHandlingModeEntry = ptrHandlingMode->GetEntryByName("NewestOnly");
+    ptrHandlingMode->SetIntValue(ptrHandlingModeEntry->GetValue());
+
+    // Exposure
+    pCam->ExposureAuto.SetValue(ExposureAuto_Continuous);
+
+    // Gain
+    pCam->GainAuto.SetValue(GainAutoEnums::GainAuto_Once);
+
+    // Gamma
+    pCam->Gamma.SetValue(1.0);
+
+    pCam->BeginAcquisition();
+}
+
+flirCamera::~flirCamera()
+{
+    pCam = nullptr;
+    flirCamList.Clear();
+    flirSystem->ReleaseInstance();
+}
+
+void flirCamera::setAutoExposure()
+{
+    pCam->ExposureAuto.SetValue(Spinnaker::ExposureAuto_Continuous);
+}
+
+void flirCamera::setManualExposure(int exposuretime = 0)
+{
+    // If no argument passed, use auto_once mode (this is the default)
+    if (exposuretime == 0)
+    {
+        pCam->ExposureAuto.SetValue(Spinnaker::ExposureAuto_Once);
+    }
+    else
+    {
+        pCam->ExposureAuto.SetValue(Spinnaker::ExposureAuto_Off);
+        pCam->ExposureMode.SetValue(Spinnaker::ExposureMode_Timed);
+        pCam->ExposureTime.SetValue(exposuretime);
+    }
+}
+
+void flirCamera::setManualGain(double value = 0)
+{
+    // If no argument passed, use auto_once mode (this is the default)
+    if (value == 0)
+    {
+        pCam->GainAuto.SetValue(Spinnaker::GainAutoEnums::GainAuto_Once);
+    }
+    else
+    {
+        pCam->GainAuto.SetValue(Spinnaker::GainAutoEnums::GainAuto_Off);
+        pCam->Gain.SetValue(value);
+    }
+}
+
+void flirCamera::setAutoGain()
+{
+    pCam->GainAuto.SetValue(Spinnaker::GainAutoEnums::GainAuto_Continuous);
+}
+
+void flirCamera::getFrame()
+{
+    Spinnaker::ImagePtr frame = nullptr;
+    Spinnaker::ImageStatus framestatus;
+    frame = pCam->GetNextImage();
+
+    framestatus = frame->GetImageStatus();
+    if (framestatus != 0)
+        printf("FRAME ERROR");
+
+    grayFrame.data = (uint8_t *)frame->GetData();
+    cv::cvtColor(grayFrame, colorFrame, cv::COLOR_GRAY2BGR);
+    frame->Release();
+}
---
+++ main.cpp
@@ -0,0 +1,296 @@
+#include "main.h"
+#include "Cameras.h"
+#include "detection.h"
+#include "globals.h"
+#include "graphics_helpers.h"
+#include "pose_estimation.h"
+#define depth_blank "025222072169"
+
+using namespace std;
+using namespace cv;
+using mjpgs = nadjieb::MJPEGStreamer;
+
+int total_hamm_hist[HAMM_HIST_MAX];
+int hamm_hist[HAMM_HIST_MAX];
+
+int cone_min_hue;
+int cone_max_hue;
+int cone_min_sat;
+int cone_max_sat;
+int cone_min_val;
+int cone_max_val;
+
+int cube_min_hue;
+int cube_max_hue;
+int cube_min_sat;
+int cube_max_sat;
+int cube_min_val;
+int cube_max_val;
+
+int main()
+{
+    /**********************************************************************************************
+     * Camera & Stream Setup *
+     *************************/
+
+    depthCamera depth(DEPTH_RED, 640, 480, 60);
+    depth.setPosOffset(1 / INCH, 5 / INCH, 23.5 / INCH, 0, -0.349);
+    // Blue camera settings
+    vector<int> params = {IMWRITE_JPEG_QUALITY, 80};
+    mjpgs streamer;
+    streamer.start(8080);
+    // Red camera settings
+    depth.setCamParams(599, 600, 334, 236);
+
+    depthCamera tagCam(DEPTH_BLUE, 640, 480, 60);
+    tagCam.setPosOffset(1 / INCH, -2 / INCH, 23.5 / INCH, 0, -0.349);
+    tagCam.setCamParams(608, 608, 323, 245);
+    tagCam.setDistCoeffs(0.09116903370720442, 0.2567349843314421, -0.003936586357063021,
+                         0.001658039412119442, -1.633408316803933);
+
+    /**********************************************************************************************
+     * AprilTags Setup *
+     *******************/
+
+    // Initialize tag detector with options
+    apriltag_family_t *tf = NULL;
+    tf = tag16h5_create();
+
+    apriltag_detector_t *td = apriltag_detector_create();
+    apriltag_detector_add_family_bits(td, tf, HAMMING_NUMBER);
+
+    td->quad_decimate = QUAD_DECIMATE;
+    td->quad_sigma = QUAD_SIGMA;
+    td->nthreads = NTHREADS;
+    td->debug = APRIL_DEBUG;
+    td->refine_edges = REFINE_EDGES;
+
+    memset(total_hamm_hist, 0, sizeof(int) * HAMM_HIST_MAX);
+
+    /**********************************************************************************************
+     * Network Tables Setup *
+     ************************/
+
+    // Create networktables instan8ce and a table for vision
+    nt::NetworkTableInstance nt_inst = nt::NetworkTableInstance::GetDefault();
+
+    // Setup networktable client
+    nt_inst.StartClient4("jetson client");
+    nt_inst.SetServerTeam(2584);
+    nt_inst.StartDSClient();
+    nt_inst.SetServer("host", NT_DEFAULT_PORT4);
+
+    // Create tables
+    shared_ptr<nt::NetworkTable> visionTbl = nt_inst.GetTable("vision");
+
+    // Make a sanity check topic and an entry to publish/read from it; set initial
+    // value
+    nt::IntegerTopic sanitycheck = visionTbl->GetIntegerTopic("sanitycheck");
+    nt::IntegerEntry sanitycheckEntry = sanitycheck.GetEntry(0, {.periodic = 0.01});
+    sanitycheckEntry.Set(1);
+
+    // Connected topic
+    nt::BooleanTopic connectedTopic = visionTbl->GetBooleanTopic("connected");
+    nt::BooleanSubscriber connectedSub = connectedTopic.Subscribe(false);
+
+    // Other vision topics
+    nt::DoubleArrayTopic robot_pose_Topic = visionTbl->GetDoubleArrayTopic("poseArray");
+    nt::DoubleArrayEntry robot_pose_Entry = robot_pose_Topic.GetEntry({});
+
+    nt::BooleanTopic see_cones_Topic = visionTbl->GetBooleanTopic("seeCones");
+    nt::BooleanSubscriber see_cones_Sub = see_cones_Topic.Subscribe({true});
+
+    nt::BooleanTopic see_cubes_Topic = visionTbl->GetBooleanTopic("seeCubes");
+    nt::BooleanSubscriber see_cubes_Sub = see_cubes_Topic.Subscribe({true});
+
+    nt::BooleanTopic see_tags_Topic = visionTbl->GetBooleanTopic("seeTags");
+    nt::BooleanSubscriber see_tags_Sub = see_tags_Topic.Subscribe({true});
+
+    nt::DoubleArrayTopic cone_pos_Topic = visionTbl->GetDoubleArrayTopic("conePos");
+    nt::DoubleArrayPublisher cone_pos_Pub = cone_pos_Topic.Publish();
+
+    nt::DoubleArrayTopic cube_pos_Topic = visionTbl->GetDoubleArrayTopic("cubePos");
+    nt::DoubleArrayPublisher cube_pos_Pub = cube_pos_Topic.Publish();
+
+    nt::DoubleArrayTopic pole_pos_Topic = visionTbl->GetDoubleArrayTopic("polePos");
+    nt::DoubleArrayPublisher pole_pos_Pub = pole_pos_Topic.Publish();
+
+    nt::BooleanTopic has_cone_Topic = visionTbl->GetBooleanTopic("hasCone");
+    nt::BooleanEntry has_cone_Entry = has_cone_Topic.GetEntry({true});
+
+    /**********************************************************************************************
+     * GET FILTER PARAMETERS *
+     *************************/
+    vector<int> coneParams;
+    vector<int> cubeParams;
+
+    string line;
+    ifstream coneFile("../../../tune-vision/cone-params.txt");
+    for (int i = 0; i < 6; i++)
+    {
+        if (coneFile)
+            getline(coneFile, line);
+        if (line != "")
+            coneParams.push_back(stoi(line));
+    }
+    if (coneParams.size() > 0)
+    {
+        cone_min_hue = coneParams[0];
+        cout << "cone_min_hue " << cone_min_hue << endl;
+        cone_max_hue = coneParams[1];
+        cout << "cone_max_hue " << cone_max_hue << endl;
+        cone_min_sat = coneParams[2];
+        cout << "cone_min_sat " << cone_min_sat << endl;
+        cone_max_sat = coneParams[3];
+        cout << "cone_max_sat " << cone_max_sat << endl;
+        cone_min_val = coneParams[4];
+        cout << "cone_min_val " << cone_min_val << endl;
+        cone_max_val = coneParams[5];
+        cout << "cone_max_val " << cone_max_val << endl << endl;
+    }
+
+    ifstream cubeFile("../../../tune-vision/cube-params.txt");
+    for (int i = 0; i < 6; i++)
+    {
+        if (coneFile)
+            getline(cubeFile, line);
+        if (line != "")
+            cubeParams.push_back(stoi(line));
+    }
+    if (cubeParams.size() > 0)
+    {
+        cube_min_hue = cubeParams[0];
+        cout << "cube_min_hue " << cube_min_hue << endl;
+        cube_max_hue = cubeParams[1];
+        cout << "cube_max_hue " << cube_max_hue << endl;
+        cube_min_sat = cubeParams[2];
+        cout << "cube_min_sat " << cube_min_sat << endl;
+        cube_max_sat = cubeParams[3];
+        cout << "cube_max_sat " << cube_max_sat << endl;
+        cube_min_val = cubeParams[4];
+        cout << "cube_min_val " << cube_min_val << endl;
+        cube_max_val = cubeParams[5];
+        cout << "cube_max_val " << cube_max_val << endl << endl << endl;
+    }
+    /**********************************************************************************************
+     * THE LOOP *
+     ************/
+    int counter = 2;
+    double poseNum = 0;
+    double coneNum = 0;
+
+    while (true)
+    {
+        if (!connectedSub.Get())
+        {
+            printf("Reconnecting...\n");
+            nt_inst.StartClient4("jetson client");
+            nt_inst.SetServerTeam(2584);
+            nt_inst.StartDSClient();
+            nt_inst.SetServer("host", NT_DEFAULT_PORT4);
+        }
+
+        errno = 0;
+        memset(hamm_hist, 0, sizeof(hamm_hist));
+
+        // Make sure networktables is working
+        sanitycheckEntry.Set(counter);
+        counter++;
+
+        depth.getFrame();
+        chrono::time_point frameTime = chrono::steady_clock::now();
+        tagCam.getFrame();
+
+        if (errno == EAGAIN)
+        {
+            printf("Unable to create the %d threads requested.\n", td->nthreads);
+            continue;
+        }
+
+        if (see_tags_Sub.Get())
+        {
+            std::vector<robot_position> poses =
+                getPoses(tagCam.grayFrame, tagCam.colorFrame, &tagCam.info, td);
+
+            for (unsigned int i = 0; i < poses.size(); i++)
+            {
+                robot_position pos = poses[i];
+                cout << "X: " << pos.x * INCH << endl;
+                cout << "Y: " << pos.y * INCH << endl;
+                cout << "Z: " << pos.z * INCH << endl << endl;
+                cout << "Theta: " << pos.theta << endl;
+
+                double micros = time_since(frameTime);
+                vector<double> poseVector = {pos.x, pos.y, pos.z, pos.theta, micros, poseNum};
+                robot_pose_Entry.Set(poseVector);
+                nt_inst.Flush();
+                poseNum++;
+            }
+        }
+
+        if (see_cones_Sub.Get())
+        {
+            // Print & send cone info
+            pair<double, double> conePos = depth.findCones();
+            cout << "Cone X: " << conePos.first << endl;
+            cout << "Cone Y: " << conePos.second << endl;
+            double micros = time_since(frameTime);
+            vector<double> coneVector = {conePos.first, conePos.second, micros, coneNum};
+            cone_pos_Pub.Set(coneVector);
+        }
+
+        if (see_cubes_Sub.Get())
+        {
+            // Print & send cube info
+            pair<double, double> cubePos = depth.findCubes();
+            cout << "Cube X: " << cubePos.first << endl;
+            cout << "Cube Y: " << cubePos.second << endl << endl;
+            double micros = time_since(frameTime);
+            vector<double> cubeVector = {cubePos.first, cubePos.second, micros, coneNum};
+            cube_pos_Pub.Set(cubeVector);
+        }
+
+        // Print & send pole info
+        /*
+        pair<double, double> polePos = depth.findPoles();
+        cout << "Pole X: " << polePos.first << endl;
+        cout << "Pole Y: " << polePos.second << endl;
+        ms = time_since(frameTime);
+        vector<double> poleVector = {polePos.first, polePos.second, ms, id}
+        pole_pos_Pub.Set(poleVector);
+        */
+
+        nt_inst.Flush();
+
+        // Graphics stuff
+        // drawMargins(depth.colorFrame);
+
+        // streamer
+        // http://localhost:8080/bgr
+        vector<uchar> buf_bgr;
+        imencode(".jpg", depth.colorFrame, buf_bgr, params);
+        streamer.publish("/depth", string(buf_bgr.begin(), buf_bgr.end()));
+
+        vector<uchar> buf_tags;
+        imencode(".jpg", tagCam.colorFrame, buf_tags, params);
+        streamer.publish("/tags", string(buf_tags.begin(), buf_tags.end()));
+
+        if (waitKey(10) == 'q')
+            break;
+    }
+
+    apriltag_detector_destroy(td);
+    tag16h5_destroy(tf);
+    destroyAllWindows();
+    streamer.stop();
+
+    return 0;
+}
+
+// Return time elapsed since passed time_point in microseconds
+double time_since(std::chrono::time_point<std::chrono::steady_clock> start)
+{
+    chrono::time_point end = chrono::steady_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+    return (double)duration.count();
+}
